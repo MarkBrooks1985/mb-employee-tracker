b2d8ca23dc4fdcabcc4f79889570952eda67ffa1
Alynn'nari Alish'sundi <alynnnari@gmail.com>
2022-08-27 14:20:20 +0930

    Second commit, started work on the application

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // get the client
-const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+function startEmpDB() {
+    type: "list",
+    name: "action",
+    choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "View All Employees by Department",
+        "View All Employees by Roles",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee",
+        "Exit"
+    ]
+};
